ac631d5784bff5890205bbb9581df63cdff533e8
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-31 08:33:47 -0300

    Add files via upload

---
+++ enumerations and compositions II/src/entities/Department.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Department {
+
+	private String name;
+
+	public Department() {
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName1() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
